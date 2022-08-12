3d4357cbe1c2fc20a781eb15f7cd3500e61bb5fb
Martin-DeckerMed <105661802+Martin-DeckerMed@users.noreply.github.com>
2022-08-12 16:26:58 -0400

    Added more discoball elements

--- src/components/DiscoBall.js
+++ src/components/DiscoBall.js
@@ -4,12 +4,15 @@ const DiscoBallRope = () => {
  return (<div></div>)
 }
 
+const DiscoBallSphere = () => {
+  return (<div></div>)
+}
 const DiscoBall = () => {
   return (
-      <>
+      <div className="discoBall">
         <DiscoBallRope />
         <DiscoBallSphere />
-      </>
+      </div>
     );
 }
 
