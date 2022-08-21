d23779e3c128a4966e68e2171241f9693e700eec
Martin-DeckerMed <105661802+Martin-DeckerMed@users.noreply.github.com>
2022-08-21 17:53:47 -0400

    Adding separate animation component:

--- src/components/DiscoLights.js
+++ src/components/DiscoLights.js
@@ -1,8 +1,10 @@
 import './DiscoLights.scss';
+import WhiteEllipse from "../assets/white-ellipse.svg";
 
 const DiscoLights = () => {
   return (
       <div className="discoLights">
+        <div className="discoLights-animation"></div>
       </div>
     );
 }
