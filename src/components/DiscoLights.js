1d02cf1121438ce859ab2253b426b3dfb4e00016
Martin-DeckerMed <105661802+Martin-DeckerMed@users.noreply.github.com>
2022-08-30 10:55:23 -0400

    Added lights graphic, basic circles implemented

--- src/components/DiscoLights.js
+++ src/components/DiscoLights.js
@@ -1,9 +1,33 @@
 import './DiscoLights.scss';
-import WhiteEllipse from "../assets/white-ellipse.svg";
+import {ReactComponent as WhiteEllipse} from "../assets/white-ellipse.svg";
+
+const DiscoCircle = ({layer}) => {
+  const height = `${layer * 20}px`;
+  const numCircles = layer * 8;
+
+  let styles = `height: ${height}px; position: absolute;`
+
+  return [...Array(numCircles).keys()].map((_, index) => {
+    styles = {
+      height: `${height}px`,
+      position: 'absolute',
+      transform: `rotate(${360 * ((index + 1) / numCircles - 1)}deg)`,
+    }
+    return (
+      <div style={styles}>
+        <WhiteEllipse />
+        <WhiteEllipse />
+        <WhiteEllipse />
+        <WhiteEllipse />
+      </div>
+    )
+  })
+}
 
 const DiscoLights = () => {
   return (
       <div className="discoLights">
+        <DiscoCircle layer={5} />
         <div className="discoLights-animation"></div>
       </div>
     );
